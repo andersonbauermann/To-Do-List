3607f7fcf06541d35b8da0e4bcfe12ec4cdb16d6
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2023-12-06 07:30:08 -0300

    Create Task component
    Criado o componente Task, e implementado o style css

--- todo-list/src/components/TaskGrid.vue
+++ todo-list/src/components/TaskGrid.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="tasks">
+    <div class="tasks-grid">
         <template>
-            <div v-for="task in tasks" :key="task.name">
-                {{ task.name }}
-            </div>
+            <task v-for="task in tasks" :key="task.name" :task="task">
+            </task>
         </template>
     </div>
 </template>
 
 <script>
+
+import Task from './Task.vue';
+
 export default {
+    components: { Task },
     props: {
         tasks: {
             type: Array,
